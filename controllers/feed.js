92dc64bb322f8d90312de0ec39727ad1ea065f28
KFC <kfclts@gmail.com>
2019-05-16 23:04:17 +0800

    Adding Server Side Validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,7 @@
+const { validationResult } = require('express-validator/check');
+
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -16,6 +20,13 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: 'Validation failed, entered data is incorrect.',
+      errors: errors.array()
+    });
+  }
   const title = req.body.title;
   const content = req.body.content;
   // Create post in db
